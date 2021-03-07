9e0d13f8007a91b04e6f5964c7ea0f9913bbbab9
Deepak <ds9199027@gmail.com>
2021-03-07 12:54:59 +0530

    image update

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Deepak Are you Ready Yes!! <img src= "https://myimagest.blob.core.windows.net/mytest/deepak.jpeg" />"
+    return "Hello, Deepak Are you Ready Yes!! <img src='https://myimagest.blob.core.windows.net/mytest/deepak.jpeg' />"
